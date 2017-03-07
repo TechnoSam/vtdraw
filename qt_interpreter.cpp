91a6d77f324ff0d68c98a1a077df19f0e83e9ab9
TechnoSam <technosam4jc@gmail.com>
2017-03-07 00:50:29 -0500

    Update QtInterpreter module w/ required functions
    Stole the constructor from the Settings exercise

---
+++ qt_interpreter.cpp
@@ -0,0 +1,37 @@
+// File: qt_interpreter.cpp
+// Author: Samuel McFalls
+
+#include "qt_interpreter.hpp"
+
+#include <QObject>
+#include <QGraphicsItem>
+
+QtInterpreter::QtInterpreter(QObject * parent): QObject(parent) {
+
+
+
+}
+
+void QtInterpreter::drawGraphic(QGraphicsItem * item) {
+
+
+
+}
+
+void QtInterpreter::info(QString message) {
+
+
+
+}
+
+void QtInterpreter::error(QString message) {
+
+
+
+}
+
+void QtInterpreter::parseAndEvaluate(QString entry) {
+
+
+
+}
---
+++ qt_interpreter.hpp
@@ -0,0 +1,35 @@
+// File: qt_interpreter.hpp
+// Author: Samuel McFalls
+
+#ifndef QT_INTERPRETER_H
+#define QT_INTERPRETER_H
+
+#include <QObject>
+#include <QGraphicsItem>
+
+class QtInterpreter: QObject {
+
+public:
+
+	// Default construct an QtInterpreter with the default environment and an empty AST
+	QtInterpreter(QObject * parent = nullptr);
+
+signals:
+
+	// a signal emitting a graphic to be drawn as a pointer
+	void drawGraphic(QGraphicsItem * item);
+
+	// a signal emitting an informational message
+	void info(QString message);
+
+	// a signal emitting an error message
+	void error(QString message);
+
+public slots:
+
+	// a public slot that accepts and expression string and parses/evaluates it
+	void parseAndEvaluate(QString entry);
+
+};
+
+#endif
