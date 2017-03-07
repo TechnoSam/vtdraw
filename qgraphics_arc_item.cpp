8717ba532eeb6d4f11c0caefdffb2c017d4f7f8c
TechnoSam <technosam4jc@gmail.com>
2017-03-07 01:08:09 -0500

    Update QGraphicsArcItem module w/ required funcs
    Unsure how to handle the default widget value of 0

---
+++ qgraphics_arc_item.cpp
@@ -0,0 +1,12 @@
+// File: qgraphics_arc_item.cpp
+// Author: Samuel McFalls
+
+#include "qgraphics_arc_item.hpp"
+
+#include <QGraphicsEllipseItem>
+
+void QGraphicsArcItem::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget) {
+
+
+
+}
---
+++ qgraphics_arc_item.hpp
@@ -0,0 +1,15 @@
+// File: qgraphics_arc_item.hpp
+// Author: Samuel McFalls
+
+#ifndef QGRAPHICS_ARC_ITEM_H
+#define QGRAPHICS_ARC_ITEM_H
+
+#include <QGraphicsEllipseItem>
+
+class QGraphicsArcItem : QGraphicsEllipseItem {
+
+	void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0);
+
+};
+
+#endif
