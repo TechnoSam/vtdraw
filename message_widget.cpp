400070c1e3ae6e25e37b86c66f32909c3d66f6d0
TechnoSam <technosam4jc@gmail.com>
2017-03-07 01:17:58 -0500

    Update MessageWidget module w/ required functions

---
+++ message_widget.cpp
@@ -0,0 +1,24 @@
+// File: message_widget.cpp
+// Author: Samuel McFalls
+
+#include "message_widget.hpp"
+
+#include <QWidget>
+
+MessageWidget::MessageWidget(QWidget * parent): QWidget(parent) {
+
+
+
+}
+
+void MessageWidget::info(QString message) {
+
+
+
+}
+
+void MessageWidget::error(QString message) {
+
+
+
+}
---
+++ message_widget.hpp
@@ -0,0 +1,28 @@
+// File: message_widget.hpp
+// Author: Samuel McFalls
+
+#ifndef MESSAGE_WIDGET_H
+#define MESSAGE_WIDGET_H
+
+#include <QWidget>
+
+class MessageWidget: public QWidget {
+Q_OBJECT
+
+public:
+
+	// Default construct a MessageWidget displaying no text
+	MessageWidget(QWidget * parent = nullptr);
+
+public slots:
+
+	// a public slot accepting an informational message to display, clearing any error formatting
+	void info(QString message);
+
+	// a public slot accepting an error message to display as selected text highlighted with a red background.
+	void error(QString message);
+
+
+};
+
+#endif
