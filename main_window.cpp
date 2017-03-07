5f08cf9148f38ee8cd4f9ebf04e58d67ad934a13
TechnoSam <technosam4jc@gmail.com>
2017-03-07 13:01:59 -0500

    [#1] Add minimal functionality to REPLWidget
    REPLWidget contains a label and a QLineEdit that allows one line text
    editing. No other functionality is implemented.

--- main_window.cpp
+++ main_window.cpp
@@ -4,10 +4,17 @@
 #include "main_window.hpp"
 
 #include <QWidget>
+#include <QLayout>
 
 MainWindow::MainWindow(QWidget * parent) : QWidget(parent) {
 
-	
+	REPLWidget * repl = new REPLWidget(this);
+
+	QVBoxLayout * layout = new QVBoxLayout(this);
+
+	layout->addWidget(repl);
+
+	this->setLayout(layout);
 
 }
 
--- repl_widget.cpp
+++ repl_widget.cpp
@@ -5,9 +5,22 @@
 
 #include <QWidget>
 #include <QLineEdit>
+#include <QString>
+#include <QLabel>
+#include <QLayout>
 
 REPLWidget::REPLWidget(QWidget * parent) : QWidget(parent) {
 
+	QLabel * prompt = new QLabel(this);
+	prompt->setText("vtscript>");
 
+	QLineEdit * input = new QLineEdit(this);
+
+	QHBoxLayout * layout = new QHBoxLayout(this);
+
+	layout->addWidget(prompt);
+	layout->addWidget(input);
+
+	this->setLayout(layout);
 
 }
