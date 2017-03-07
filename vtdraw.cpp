e801f9bedec688e8b0ac8298f1aba7afa4840026
TechnoSam <technosam4jc@gmail.com>
2017-03-07 02:06:18 -0500

    Update vtdraw.cpp to have a skeleton

---
+++ vtdraw.cpp
@@ -0,0 +1,18 @@
+// File: vtdraw.cpp
+// Author: Samuel McFalls
+
+#include <QApplication>
+
+#include "main_window.hpp"
+
+int main(int argc, char *argv[]) {
+
+	QApplication app(argc, argv);
+
+	MainWindow mainWindow;
+
+	mainWindow.show();
+
+	return app.exec();
+
+}
