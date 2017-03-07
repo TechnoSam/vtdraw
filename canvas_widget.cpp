584fbd0257873ee8bb7cf2d8df6f60cdbd371d9f
TechnoSam <technosam4jc@gmail.com>
2017-03-07 15:23:17 -0500

    [#2] Add minimal functionality to CanvasWidget
    CanvasWidget contains a QGraphicsScene and a QGraphicsView. As a
    placeholder, the text "Hello Lyoko!" has been inserted into the scene.
    No other functionality is implemented.

--- canvas_widget.cpp
+++ canvas_widget.cpp
@@ -7,10 +7,21 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QGraphicsItem>
+#include <QLayout>
 
 CanvasWidget::CanvasWidget(QWidget * parent) {
 
+	QGraphicsScene * scene = new QGraphicsScene(this);
+	scene->addText("Hello Lyoko!");
 
+	QGraphicsView * view = new QGraphicsView(scene);
+	view->show();
+
+	QHBoxLayout * layout = new QHBoxLayout(this);
+
+	layout->addWidget(view);
+
+	this->setLayout(layout);
 
 }
 
--- main_window.cpp
+++ main_window.cpp
@@ -9,11 +9,13 @@
 MainWindow::MainWindow(QWidget * parent) : QWidget(parent) {
 
 	MessageWidget * message = new MessageWidget(this);
+	CanvasWidget * canvas = new CanvasWidget(this);
 	REPLWidget * repl = new REPLWidget(this);
 
 	QVBoxLayout * layout = new QVBoxLayout(this);
 
 	layout->addWidget(message);
+	layout->addWidget(canvas);
 	layout->addWidget(repl);
 
 	this->setLayout(layout);
