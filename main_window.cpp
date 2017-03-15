a9010125b6ce942d74bcb1c35acea465f7ec2f6c
TechnoSam <technosam4jc@gmail.com>
2017-03-14 22:09:26 -0400

    Add file preloading from command line

--- main_window.cpp
+++ main_window.cpp
@@ -5,6 +5,9 @@
 
 #include <QWidget>
 #include <QLayout>
+#include <QDebug>
+
+#include <fstream>
 
 #include "qt_interpreter.hpp"
 
@@ -33,6 +36,35 @@ MainWindow::MainWindow(QWidget * parent) : QWidget(parent) {
 
 MainWindow::MainWindow(std::string filename, QWidget * parent) : QWidget(parent) {
 
+	MessageWidget * message = new MessageWidget(this);
+	CanvasWidget * canvas = new CanvasWidget(this);
+	REPLWidget * repl = new REPLWidget(this);
 
+	QVBoxLayout * layout = new QVBoxLayout(this);
+
+	layout->addWidget(message);
+	layout->addWidget(canvas);
+	layout->addWidget(repl);
+
+	this->setLayout(layout);
+
+	QtInterpreter * interp = new QtInterpreter(this);
+
+	QObject::connect(repl, SIGNAL(lineEntered(QString)), interp, SLOT(parseAndEvaluate(QString)));
+	QObject::connect(interp, SIGNAL(info(QString)), message, SLOT(info(QString)));
+	QObject::connect(interp, SIGNAL(error(QString)), message, SLOT(error(QString)));
+	QObject::connect(interp, SIGNAL(drawGraphic(QGraphicsItem *)), canvas, SLOT(addGraphic(QGraphicsItem *)));
+
+	std::ifstream ifs(filename);
+	if (!ifs.good()) {
+		qDebug() << "File " << QString::fromStdString(filename) <<
+			"does not exist or could not be opened for reading. Ignoring...";
+	}
+	else {
+		std::stringstream buffer;
+		buffer << ifs.rdbuf();
+		QString program = QString::fromStdString(buffer.str());
+		repl->lineEntered(program);
+	}
 
 }
--- vtdraw.cpp
+++ vtdraw.cpp
@@ -9,10 +9,18 @@ int main(int argc, char *argv[]) {
 
 	QApplication app(argc, argv);
 
-	MainWindow mainWindow;
-	mainWindow.setMinimumSize(800, 600);
+	MainWindow * mainWindow;
 
-	mainWindow.show();
+	if (argc == 2) {
+		mainWindow = new MainWindow(argv[1]);
+	}
+	else {
+		mainWindow = new MainWindow();
+	}
+
+	mainWindow->setMinimumSize(800, 600);
+
+	mainWindow->show();
 
 	return app.exec();
 
